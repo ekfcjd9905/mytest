e18e5f69b7d42ef46f9814a381f06610eb1fce42
ekfcjd9905 <ekfcjd9905@gmail.com>
2025-11-20 13:30:33 +0900

    Update mytest_streamlit.py

--- mytest_streamlit.py
+++ mytest_streamlit.py
@@ -1,1 +1,9 @@
+import streamlit as st
+st.title("안녕~!! 나 :blue[이상기 교수]야!! :sunglasses:")
 
+st.divider()
+
+st.write("연애는 조건만남이야 응?! 알았어? 연애 안하고 있어? 응?! 그러면 연애를 못하는 거야~ 병신아~ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
+
+st.image("https://search.pstatic.net/common/?src=http%3A%2F%2Fimgnews.naver.net%2Fimage%2F082%2F2020%2F06%2F01%2F0001006414_001_20200601192209518.jpg&type=sc960_832",
+         use_container_width=True)
